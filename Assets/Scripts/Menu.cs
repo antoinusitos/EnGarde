d94976cc46ffdcb8f971cd6a7944cca87bfd38b9
antoinusitos <a.gargasson@rubika-edu.com>
2018-03-04 17:32:07 +0100

    Deck Building in progress

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject mainMenu = null;
+    public GameObject DeckBuildMenu = null;
+
     public void LaunchGame()
     {
         SceneManager.LoadScene(1);
@@ -12,6 +15,13 @@ public class Menu : MonoBehaviour
 
     public void LaunchDeckBuilding()
     {
+        DeckBuildMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
 
+    public void BackToMenu()
+    {
+        DeckBuildMenu.SetActive(false);
+        mainMenu.SetActive(true);
     }
 }
